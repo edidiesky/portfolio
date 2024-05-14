2b23ed660919565f321bd1142230d526708ad5cb
edidiesky <essienedidiong1000@gmail.com>
2024-05-13 18:59:18 -0700

    new fixes

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,26 +1,44 @@
 import React, {  useRef, useState } from "react";
-import { motion } from "framer-motion";
 import Link from "next/link";
-import { scaleAnimations } from "@/constants/utils/framer";
+import { opacity, slideup, slideup2, slideup3 } from "@/constants/utils/framer";
+import { motion, useInView } from "framer-motion";
 const Footer = () => {
   const [active, setActive] = useState(false);
   const [mouseposition, setMousePosition] = useState({
     active: false,
     index: 0,
   });
-  const labelRef = useRef(null);
-
-
+  const container2 = useRef(null);
+  const inView2 = useInView(container2, {
+    margin: "0px 100px -50px 0px",
+  });
+const heroWords2 = `LET US BUILD THAT PR0JECT TOGETHER`;
   return (
     <>
       <div className="relative min-h-[40vh] flex flex-col justify-between py-8 pt-32">
         <div className="w-[90%] md:w-[85%] max-w-custom mx-auto flex flex-col items-center gap-24">
           <div className="flex w-full pb-28 lg:pb-24 border-b border-[rgba(0,0,0,.1)] relative items-center gap-2">
             <h2
-              ref={labelRef}
-              className="text-[90px] sm:w-[700px] leading-[1] lg:text-[150px] text-dark"
+              ref={container2}
+              className="text-[90px] sm:w-[700px] flex flex-wrap gap-[8px]  leading-[.9] lg:text-[150px] text-dark"
             >
-              LET US BUILD THAT PR0JECT TOGETHER
+              {heroWords2.split(" ").map((x, index) => {
+                return (
+                  <span
+                    key={index}
+                    className="flex hide relative items-center justify-start"
+                  >
+                    <motion.span
+                      variants={slideup2}
+                      custom={index}
+                      initial="initial"
+                      animate={inView2 ? "animate" : "exit"}
+                    >
+                      {x}
+                    </motion.span>
+                  </span>
+                );
+              })}
             </h2>
             <Link
               href={
@@ -32,11 +50,13 @@ const Footer = () => {
             </Link>
           </div>
           <div className="w-full pt-8 flex flex-col items-center sm:flex-row justify-between gap-16">
-            <h4 className="w-full text-xl pb-8 font-portfolio_bold1 border-b border-[rgba(0,0,0,.1)] sm:pb-0 sm:border-b-0 text-dark">
+            <h4 className="w-full text-lg pb-8 font-portfolio_bold1 border-b border-[rgba(0,0,0,.1)] sm:pb-0 sm:border-b-0 text-dark">
               © Code By Victor Essien 2024
             </h4>
-            <div className="flex font-portfolio_bold1 flex-col justify-end gap-4">
-              <h4 className="text-2xl text-dark">SOCIALS</h4>
+            <div className=" flex font-portfolio_bold1 flex-col md:justify-end gap-2">
+              <h4 className="text-lg text-dark font-bold font-portfolio_bold2">
+                SOCIALS
+              </h4>
               <ul className="flex text-lg text-dark items-center gap-16">
                 <Link href={"https://twitter.com/edidiesky"}>Twitter</Link>
                 <Link
