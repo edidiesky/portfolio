7fe953a53e3e9298699e86d42419a7415e38af4a
edidiesky <essienedidiong1000@gmail.com>
2024-08-25 10:59:57 -0700

    new fixes

--- src/components/common/WorkList.jsx
+++ src/components/common/WorkList.jsx
@@ -6,7 +6,7 @@ const WorkList = ({ setMousePosition, index, setTab, tab, type }) => {
   if (type === "home") {
     return (
       <div className="w-full relative grid grid-cols-1 md:grid-cols-2 gap-x-8 gap-y-24 justify-between">
-        {projectdata3.slice(0, 4).map((x, index) => {
+        {projectdata3.slice(0, 8).map((x, index) => {
           return (
             <ProjectCard
               setTab={setTab}
