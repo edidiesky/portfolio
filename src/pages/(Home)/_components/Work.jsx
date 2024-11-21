a7a5d7048a83cfd96cbcf052317b233a34562621
edidiesky <essienedidiong1000@gmail.com>
2024-11-21 10:25:17 -0800

    fixed the text animations

--- src/pages/(Home)/_components/Work.jsx
+++ src/pages/(Home)/_components/Work.jsx
@@ -1,11 +1,30 @@
 "use client";
-import React, { useEffect, useRef, useState } from "react";
 import WorkList from "@/components/common/WorkList";
+import AnimateTextWord from "@/components/animations/AnimateTextWord";
+
 const Work = () => {
   return (
     <>
       <div className="py-12 w-full relative">
-        <div className="w-[90%] max-w-custom_1 flex flex-col gap-12 mx-auto">
+        <div className="w-[90%] max-w-custom_1 flex flex-col gap-12 md:gap-24 mx-auto">
+          <h4 className="text-5xl md:text-6xl uppercase w-full font-portfolio_regular leading-[1.2] titleRef text-[#fff]">
+            <div className="md:max-w-[560px]">
+              {" "}
+              <AnimateTextWord type="largeText">
+                Some of My Selected Works
+              </AnimateTextWord>{" "}
+            </div>
+            {/* <sup className="text-2xl md:text-4xl">4</sup> */}
+            <div className="w-full pt-3 lg:grid-cols-2 grid gap-4">
+              <div className="w-full"></div>
+              <span className="block text-xl text_background max-w-[500px] capitalize md:text-3xl">
+                <AnimateTextWord>
+                  Featured projects that have been meticulously crafted with
+                  passion to drive results and impact
+                </AnimateTextWord>
+              </span>
+            </div>
+          </h4>
           <WorkList />
         </div>
       </div>
